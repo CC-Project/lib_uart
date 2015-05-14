b58658b09bbb49f7667ac670b8a108a337ee3eca
graille <astres3000@free.fr>
2015-05-14 20:37:27 +0200

    Fex changes

--- uart.h
+++ uart.h
@@ -1,6 +1,11 @@
-#include <avr/io.h>
+#ifndef UART_H
+    #define UART_H
+    #pragma once
 
-void uart_init(uint16_t baud);
-void uart_tx_char(char c);
-void uart_tx_str(int8_t* str); // Prints a string
-void uart_newline(void); // Prints a newline on the uart
+    #include <avr/io.h>
+
+    void uart_init(uint16_t baud);
+    void uart_tx_char(char c);
+    void uart_tx_str(int8_t* str); // Prints a string
+    void uart_newline(void); // Prints a newline on the uart
+#endif
